c9a2639c6861ac6f64fd160cbefbef41cbef085c
aditya-ai00 <adityanyo39@gmail.com>
2026-02-09 13:56:25 +0530

    Disable interactables page to fix build

--- src/app/_interactables/page.tsx
+++ src/app/_interactables/page.tsx
@@ -1,19 +1,3 @@
-"use client";
-
-import {
-  Thread,
-  ThreadContent,
-  ThreadMessages,
-  ThreadInput,
-} from "@tambo-ai/react";
-
 export default function InteractablesPage() {
-  return (
-    <Thread>
-      <ThreadContent>
-        <ThreadMessages />
-        <ThreadInput />
-      </ThreadContent>
-    </Thread>
-  );
+  return null;
 }
