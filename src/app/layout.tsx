e16a4ac1de04401778ff6a2f22789ae3ca26dd81
aditya-ai00 <adityanyo39@gmail.com>
2026-02-09 13:10:14 +0530

    Fix TamboProvider apiKey

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="min-h-screen bg-white text-black">
-        <TamboProvider>
+        <TamboProvider apiKey={process.env.NEXT_PUBLIC_TAMBO_API_KEY as string}>
           {children}
         </TamboProvider>
       </body>
